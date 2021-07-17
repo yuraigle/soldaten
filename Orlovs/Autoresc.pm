72b99c9a64c7e22aa52e5573954f590e4022d836
orlov <yuraigle@gmail.com>
2021-07-17 15:13:34 +0800

    autoresc fix

--- Orlovs/Autoresc.pm
+++ Orlovs/Autoresc.pm
@@ -4,11 +4,6 @@ use List::MoreUtils qw/any first_index/;
 
 my $auto_resc = 0;
 my @f2resc = ();
-foreach my $f (@App::FRIENDS) {
-  if ($f->{PROF} =~ m/колд|чернок|лекар|волш|кудес|волх|тать|наем|кузнец|купец/) {
-    push @f2resc, $f;
-  }
-}
 
 my @rx_ime = (
   '^(.+) уклонил.?с. от атаки ',
@@ -46,51 +41,85 @@ my @rx_vin = (
 
 P::trig {
   my ($t1, $t2) = ($1, $4);
-  return unless $auto_resc;
   my $i = first_index {$_->{VIN} eq $t2} @f2resc;
   U::sendline('спасти .' . $f2resc[$i]->{IME}) if $i >= 0;
-} $Autopom::RX_BATTLE1, 'nf1000:AUTORESC';
+} $Autopom::RX_BATTLE1, '-nf1000:AUTORESC';
 
 foreach (@rx_ime) {
   P::trig {
-    my ($t1) = ($1);
-    return unless $auto_resc;
+    my $t1 = $1;
     my $i = first_index {$_->{IME} eq $t1} @f2resc;
     U::sendline('спасти .' . $f2resc[$i]->{IME}) if $i >= 0;
-  } $_, 'nf100:AUTORESC';
+  } $_, '-nf100:AUTORESC';
 }
 
 foreach (@rx_rod) {
   P::trig {
-    my ($t1) = ($1);
-    return unless $auto_resc;
+    my $t1 = $1;
     my $i = first_index {$_->{ROD} eq $t1} @f2resc;
     U::sendline('спасти .' . $f2resc[$i]->{IME}) if $i >= 0;
-  } $_, 'nf100:AUTORESC';
+  } $_, '-nf100:AUTORESC';
 }
 
 foreach (@rx_vin) {
   P::trig {
-    my ($t1) = ($1);
-    return unless $auto_resc;
+    my $t1 = $1;
     my $i = first_index {$_->{VIN} eq $t1} @f2resc;
     U::sendline('спасти .' . $f2resc[$i]->{IME}) if $i >= 0;
-  } $_, 'nf100:AUTORESC';
-}
-
-P::trig {
-  U::sendline('соск') if $auto_resc;
-} '^Ну раскорячили вы ноги по сторонам, ', 'nf1000';
-
-sub set_mode_resc_on {
-  $auto_resc = 1;
-  U::sendline('соск;отпу;гг рескаю немощных');
+  } $_, '-nf100:AUTORESC';
 }
 
 sub set_mode_resc_off {
   $auto_resc = 0;
+  P::disable('AUTORESC');
   U::sendline('гг не рескаю.');
 }
+
+sub set_mode_resc_on {
+  $auto_resc = 1;
+  P::enable('AUTORESC_UPD');
+  U::sendline('груп');
+}
+
+P::trig {
+  @f2resc = ();
+  P::disable('AUTORESC_UPD');
+  P::disable('AUTORESC_UPD1');
+} '^Но вы же не член ', '-nf1000:AUTORESC_UPD';
+
+P::trig {
+  @f2resc = ();
+  P::disable('AUTORESC_UPD');
+  P::enable('AUTORESC_UPD1');
+} '^Ваша группа состоит из:', '-nf1000:AUTORESC_UPD';
+
+P::trig {
+  my $t1 = $1;
+  my $i = first_index {$_->{IME} eq $t1} @App::FRIENDS;
+  if ($i >= 0
+      && $App::FRIENDS[$i]->{PROF} ne 'дружинник'
+      && $App::FRIENDS[$i]->{IME} ne $App::CONF->{CONNECT}->{name}
+  ) {
+    push @f2resc, $App::FRIENDS[$i];
+  }
+} '^([А-Яа-я]+)\s+\|[^|]+\|[^|]+\| (Да |Нет) \|', '-nf1000:AUTORESC_UPD1';
+
+P::trig {
+  P::disable('AUTORESC_UPD');
+  P::disable('AUTORESC_UPD1');
+
+  if ($#f2resc >= 0) {
+    my $s = '';
+    foreach my $m (@f2resc) {
+      $s .= ($s ? ', ' : '') . $m->{VIN};
+    }
+    U::sendline("гг рескаю: $s");
+    U::sendline('соск;отпу');
+    P::enable('AUTORESC');
+  } else {
+    U::sendline('гг некого тут рескать :(');
+  }
+} '^$', '-nf1000:AUTORESC_UPD1';
 
 P::alias {
   set_mode_resc_on();
@@ -99,5 +128,9 @@ P::alias {
 P::alias {
   set_mode_resc_off();
 } 'нерескай';
+
+P::trig {
+  U::sendline('соск;отпу');
+} '^Ну раскорячили вы ноги по сторонам, ', '-nf1000:AUTORESC';
 
 1;
